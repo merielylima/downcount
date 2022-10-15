8d0d9864e2b6812db87aa5b8ea5100635de553cf
Meriely Lima <meriellylima96@gmail.com>
2022-10-15 16:38:49 -0300

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ const contagemRegressiva = (tempo) => {
 
 const tempoRestante = () => {
     // 1 de janeiro de 1970
-    const dataEvento = new Date ('2022-10-22 16:15:00');
+    const dataEvento = new Date ('2022-10-22 16:30:00');
     const hoje = Date.now();
     return Math.floor((dataEvento - hoje) / 1000);
 }
